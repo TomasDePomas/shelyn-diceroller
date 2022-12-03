c86e6dd15f2652daf527eaf076b2433e3b053386
TomasdePomas <tomas@cell-0.com>
2022-12-03 16:57:01 +0100

    [added] roll class

--- app.js
+++ app.js
@@ -1,186 +1,211 @@
-import 'dotenv/config';
-import express from 'express';
+import 'dotenv/config'
+import express from 'express'
 import {
-  InteractionType,
-  InteractionResponseType,
-  InteractionResponseFlags,
-  MessageComponentTypes,
-  ButtonStyleTypes,
-} from 'discord-interactions';
-import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from './utils.js';
-import { getShuffledOptions, getResult } from './game.js';
+    InteractionType, InteractionResponseType,
+} from 'discord-interactions'
+import { VerifyDiscordRequest } from './utils.js'
 import {
-  CHALLENGE_COMMAND,
-  TEST_COMMAND,
-  HasGuildCommands,
-} from './commands.js';
+    ACT_COMMAND, ACT_HELP_COMMAND, HasGuildCommands,
+} from './commands.js'
 
 // Create an express app
-const app = express();
+const app = express()
 // Get port, or default to 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 // Parse request body and verifies incoming requests using discord-interactions package
-app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }))
 
-// Store for in-progress games. In production, you'd want to use a DB
-const activeGames = {};
+// [text : nX aDt + m aDt + m aDt + m aDt + m]
 
-/**
- * Interactions endpoint URL where Discord will send HTTP requests
- */
-app.post('/interactions', async function (req, res) {
-  // Interaction type and data
-  const { type, id, data } = req.body;
-
-  /**
-   * Handle verification requests
-   */
-  if (type === InteractionType.PING) {
-    return res.send({ type: InteractionResponseType.PONG });
-  }
-
-  /**
-   * Handle slash command requests
-   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-   */
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
-
-    // "test" guild command
-    if (name === 'test') {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: 'hello world ' + getRandomEmoji(),
-        },
-      });
+function matchShift (diceRoll, pattern, fallback = null) {
+    const matches = diceRoll.command.match(pattern)
+    if (!matches || matches.length === 0) {
+        return fallback
     }
-    // "challenge" guild command
-    if (name === 'challenge' && id) {
-      const userId = req.body.member.user.id;
-      // User's object choice
-      const objectName = req.body.data.options[0].value;
+    diceRoll.command = diceRoll.command.replace(matches[0], '').trim()
 
-      // Create active game using message ID as the game ID
-      activeGames[id] = {
-        id: userId,
-        objectName,
-      };
+    const value = matches.length === 1 ? matches[0].trim() : matches[1].trim()
 
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: `Rock papers scissors challenge from <@${userId}>`,
-          components: [
-            {
-              type: MessageComponentTypes.ACTION_ROW,
-              components: [
-                {
-                  type: MessageComponentTypes.BUTTON,
-                  // Append the game ID to use later on
-                  custom_id: `accept_button_${req.body.id}`,
-                  label: 'Accept',
-                  style: ButtonStyleTypes.PRIMARY,
-                },
-              ],
-            },
-          ],
-        },
-      });
+    if (typeof fallback === 'number') {
+        return parseInt(value)
     }
-  }
+    return value
+}
 
-  /**
-   * Handle requests from interactive components
-   * See https://discord.com/developers/docs/interactions/message-components#responding-to-a-component-interaction
-   */
-  if (type === InteractionType.MESSAGE_COMPONENT) {
-    // custom_id set in payload when sending message component
-    const componentId = data.custom_id;
+class Roll {
+    constructor () {
+        this._amount = 1
+        this._type = 20
+        this._modifier = 0
+        this._outcomes = []
+    }
 
-    if (componentId.startsWith('accept_button_')) {
-      // get the associated game ID
-      const gameId = componentId.replace('accept_button_', '');
-      // Delete message with token in request body
-      const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
-      try {
-        await res.send({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            // Fetches a random emoji to send from a helper function
-            content: 'What is your object of choice?',
-            // Indicates it'll be an ephemeral message
-            flags: InteractionResponseFlags.EPHEMERAL,
-            components: [
-              {
-                type: MessageComponentTypes.ACTION_ROW,
-                components: [
-                  {
-                    type: MessageComponentTypes.STRING_SELECT,
-                    // Append game ID
-                    custom_id: `select_choice_${gameId}`,
-                    options: getShuffledOptions(),
-                  },
-                ],
-              },
-            ],
-          },
-        });
-        // Delete previous message
-        await DiscordRequest(endpoint, { method: 'DELETE' });
-      } catch (err) {
-        console.error('Error sending message:', err);
-      }
-    } else if (componentId.startsWith('select_choice_')) {
-      // get the associated game ID
-      const gameId = componentId.replace('select_choice_', '');
+    get result () {
+        return this._outcomes.reduce((total, outcome) => total + outcome, this._modifier)
+    }
 
-      if (activeGames[gameId]) {
-        // Get user ID and object choice for responding user
-        const userId = req.body.member.user.id;
-        const objectName = data.values[0];
-        // Calculate result from helper function
-        const resultStr = getResult(activeGames[gameId], {
-          id: userId,
-          objectName,
-        });
-
-        // Remove game from storage
-        delete activeGames[gameId];
-        // Update message with token in request body
-        const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
-
-        try {
-          // Send results
-          await res.send({
-            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: { content: resultStr },
-          });
-          // Update ephemeral message
-          await DiscordRequest(endpoint, {
-            method: 'PATCH',
-            body: {
-              content: 'Nice choice ' + getRandomEmoji(),
-              components: [],
-            },
-          });
-        } catch (err) {
-          console.error('Error sending message:', err);
+    get resultString () {
+        if (this.isCrit) {
+            return `__*${this.result}*__`
         }
-      }
+        if (this.isCritFail) {
+            return `~~_${this.result}_~~`
+        }
+        return this.result
     }
-  }
-});
+
+    get isCrit () {
+        return this.result === (this._type * this._amount) + this._modifier
+    }
+
+    get isCritFail () {
+        return this.result === 1 + this._modifier
+    }
+
+    get description () {
+        const rolls = this._outcomes.join('+')
+        if (!this._modifier) {
+            return rolls + ' = ' + this.result
+        }
+        if (this._modifier > 0) {
+            return rolls + '+' + this._modifier + ' = ' + this.result
+        }
+        return rolls + this._modifier + ' = ' + this.result
+    }
+
+    setAmount (amount) {
+        this._amount = amount
+    }
+
+    setType (type) {
+        this._type = type
+    }
+
+    addModifier (modifierString) {
+        modifierString = modifierString.replaceAll(' ', '')
+        this._modifier = parseInt(modifierString)
+    }
+
+    resolve () {
+        this._outcomes = []
+        for (let i = 0; i < this._amount; i++) {
+            this._outcomes.push(Math.floor(Math.random() * this._type) + 1)
+        }
+    }
+
+    toJson () {
+        return JSON.stringify({
+            amount: this._amount,
+            isCrit: this.isCrit,
+            isCritFail: this.isCritFail,
+            type: this._type,
+            modifier: this._modifier,
+            outcomes: this._outcomes,
+            result: this.result,
+        }, null, '\t')
+    }
+
+    toString () {
+        let diceString = `${this._amount}d${this._type}`
+        if (!this._modifier) {
+            return diceString
+        }
+        if (this._modifier > 0) {
+            return diceString + '+' + this._modifier
+        }
+        return diceString + this._modifier
+    }
+}
+
+function rollDice (diceRoll) {
+    const baseRoll = diceRoll.command
+    const description = matchShift(diceRoll, /^([^:]*:)/i, '')
+    const amountOfRolls = matchShift(diceRoll, /^(\d)\W*[x*]/i, 1)
+
+    let rolls = []
+    let rolling = true
+    let lastRoll
+    while (rolling) {
+        lastRoll = diceRoll.command.trim()
+        let roll = new Roll()
+        roll.setAmount(matchShift(diceRoll, /^(\d+)(?=d)/i, 1))
+        roll.setType(matchShift(diceRoll, /^d(\d+)/, 20))
+        roll.addModifier(matchShift(diceRoll, /^([+-]\W*\d+)/, '0'))
+        rolls.push(roll)
+
+        let remainder = diceRoll.command.trim()
+        if (!remainder) {
+            rolling = false
+        }
+        if (lastRoll === remainder) {
+            return `( * * [Error](http://d# "unable to resolve ${baseRoll}")**)`
+        }
+    }
+
+    let rollTexts = new Array(amountOfRolls).fill('')
+        .map(i => {
+            rolls.forEach(roll => {
+                roll.resolve()
+                console.log(roll.toJson())
+            })
+
+            return rolls.map(roll => {
+                return `**[${roll.resultString}](http://${roll.toString()} "${roll.toString()} = ${roll.description}")**`
+            }).join(' | ')
+        })
+
+    let rollText
+    if (rollTexts.length === 1) {
+        rollText = rollTexts[0]
+    } else {
+        rollText = rollTexts.map(text => `(${text})`).join('')
+    }
+
+    return `[${description}${rollText}]`
+}
+
+
+// (**[19](http://d20  "16 +3")**)'
+// (**[~~_int:5_~~](http://d20  "1 +5")**)'
+// (**[fireball:12](http://d20 "2+5+3 +2")**)'
+// (**[12|5](http://d20 "12 | 5")**)(**[__*20*__|2](http://d20 "20 | 2")**)'
+
+app.post('/interactions', async function (req, res) {
+    const { type, data } = req.body
+
+    if (type === InteractionType.PING) {
+        return res.send({ type: InteractionResponseType.PONG })
+    }
+
+    if (type === InteractionType.APPLICATION_COMMAND) {
+        const { name } = data
+
+        switch (name) {
+            case ACT_HELP_COMMAND.name:
+                return res.send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE, data: {
+                        content: 'A few examples of the **/act** possibilities: ' + '\n\t[d20+3]\t\t\t\t\t| (**[19](http://d20  "16 +3")**)' + '\n\t[int:+5]\t\t\t\t\t | (**[~~_int:5_~~](http://d20  "1 +5")**)' + '\n\t[fireball:3d6+2]\t  | (**[fireball:12](http://d20 "2+5+3 +2")**)' + '\n\t[2x d20 1d8]\t\t    | (**[12|5](http://d20 "12 | 5")**)(**[__*20*__|2](http://d20 "20 | 2")**)',
+                    },
+                })
+
+            case ACT_COMMAND.name:
+                let content = req.body.data.options[0].value
+                const diceBlocks = [...content.matchAll(/\[([^\]]*)\]/mgi)]
+
+                diceBlocks.forEach(block => {
+                    content = content.replace(block[0], rollDice({ command: block[1] }))
+                })
+
+                return res.send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE, data: {
+                        content,
+                    },
+                })
+        }
+    }
+})
 
 app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
-
-  // Check if guild commands from commands.js are installed (if not, install them)
-  HasGuildCommands(process.env.APP_ID, process.env.GUILD_ID, [
-    TEST_COMMAND,
-    CHALLENGE_COMMAND,
-  ]);
-});
+    console.log('Listening on port', PORT)
+    HasGuildCommands(process.env.APP_ID, process.env.GUILD_ID, [ACT_COMMAND, ACT_HELP_COMMAND])
+})
--- commands.js
+++ commands.js
@@ -1,82 +1,63 @@
-import { getRPSChoices } from './game.js';
-import { capitalize, DiscordRequest } from './utils.js';
+import { DiscordRequest } from './utils.js'
 
-export async function HasGuildCommands(appId, guildId, commands) {
-  if (guildId === '' || appId === '') return;
+export async function HasGuildCommands (appId, guildId, commands) {
+    if (guildId === '' || appId === '') return
 
-  commands.forEach((c) => HasGuildCommand(appId, guildId, c));
+    await removeOldCommands(appId, guildId)
+    commands.forEach((c) => InstallGuildCommand(appId, guildId, c))
 }
 
-// Checks for a command
-async function HasGuildCommand(appId, guildId, command) {
-  // API endpoint to get and post guild commands
-  const endpoint = `applications/${appId}/guilds/${guildId}/commands`;
-
-  try {
-    const res = await DiscordRequest(endpoint, { method: 'GET' });
-    const data = await res.json();
-
-    if (data) {
-      const installedNames = data.map((c) => c['name']);
-      // This is just matching on the name, so it's not good for updates
-      if (!installedNames.includes(command['name'])) {
-        console.log(`Installing "${command['name']}"`);
-        InstallGuildCommand(appId, guildId, command);
-      } else {
-        console.log(`"${command['name']}" command already installed`);
-      }
+async function removeOldCommands (appId, guildId) {
+    const endpoint = `applications/${appId}/guilds/${guildId}/commands`
+    try {
+        const res = await DiscordRequest(endpoint, { method: 'GET' })
+        const data = await res.json()
+        if (data) {
+            for (const installed of data) {
+                await DeleteGuildCommand(appId, guildId, installed)
+            }
+        }
+    } catch (err) {
+        console.error(err)
     }
-  } catch (err) {
-    console.error(err);
-  }
 }
 
 // Installs a command
-export async function InstallGuildCommand(appId, guildId, command) {
-  // API endpoint to get and post guild commands
-  const endpoint = `applications/${appId}/guilds/${guildId}/commands`;
-  // install command
-  try {
-    await DiscordRequest(endpoint, { method: 'POST', body: command });
-  } catch (err) {
-    console.error(err);
-  }
+export async function InstallGuildCommand (appId, guildId, command) {
+    console.log(`Installing "${command['name']}"`)
+    const endpoint = `applications/${appId}/guilds/${guildId}/commands`
+    try {
+        await DiscordRequest(endpoint, { method: 'POST', body: command })
+    } catch (err) {
+        console.error(err)
+    }
 }
 
-// Get the game choices from game.js
-function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
-
-  for (let choice of choices) {
-    commandChoices.push({
-      name: capitalize(choice),
-      value: choice.toLowerCase(),
-    });
-  }
-
-  return commandChoices;
+// Deletes a command
+export async function DeleteGuildCommand (appId, guildId, command) {
+    console.log(`Deleting "${command['name']}"`)
+    const endpoint = `applications/${appId}/guilds/${guildId}/commands/${command.id}`
+    try {
+        await DiscordRequest(endpoint, { method: 'DELETE', body: command })
+    } catch (err) {
+        console.error(err)
+    }
 }
 
-// Simple test command
-export const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic guild command',
-  type: 1,
-};
+export const ACT_COMMAND = {
+    name: 'act',
+    description: 'Enter a description of your action with dice roll blocks (for example: [d20 + 2], [2d4+2], [int:6])',
+    type: 1,
+    options: [{
+        type: 3,
+        name: 'action',
+        required: true,
+        description: 'Describe your action',
+    }],
+}
 
-// Command containing options
-export const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
-  options: [
-    {
-      type: 3,
-      name: 'object',
-      description: 'Pick your object',
-      required: true,
-      choices: createCommandChoices(),
-    },
-  ],
-  type: 1,
-};
+export const ACT_HELP_COMMAND = {
+    name: 'act-help',
+    description: 'List the possibilities of the /act command',
+    type: 1,
+}
